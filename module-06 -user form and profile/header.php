f6d1cedce82a5a5e38d449d1a75a9efe506f1ad1
Pobitro Mondal <43313135+mkpobitro@users.noreply.github.com>
2023-03-13 22:50:32 +0600

    module 6 user form and profile basic project

---
+++ module-06 -user form and profile/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php if (isset($title)) {echo $title;} ?></title>
+    <!-- Milligram CSS -->
+    </span><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"><span class="pln">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <?php 
+    if("users.php"){
+    }
+    
+    ?>
+    <div class="form-container">
+    <a class="button" href="index.php"><span class="pln">User Form</span></a>
+    <a href="/users.php" class="button button-outline"><span class="pln">User Profile</span></a><span class="pln">
+    </span>
